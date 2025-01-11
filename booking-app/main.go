d459e470e6019f0128844dc6c909917441e6ac69
Abdul Wasiue <channarwasiue@yahoo.com>
2025-01-11 18:56:20 +0500

    Variable and Data types in Golang

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello ")
+
+	var conferenceName = "Go Conference"
+
+	const conferenceTickets = 100
+
+	fmt.Printf("Total Tickets %v Available\n", conferenceTickets)
+
+	fmt.Printf("Welcome To %v Booking App\n", conferenceName)
+
+	var userName string
+	var userTicket int
+	fmt.Print("Enter Your Name: ")
+	fmt.Scan(&userName) //& is used to get the address of the variable. It is pointer to the variable address in memmory
+
+	fmt.Print("Enter Number of Tickets: ")
+	fmt.Scan(&userTicket)
+
+	fmt.Printf("Thank You for %v booking %v tickets", userName, userTicket)
+
 }
